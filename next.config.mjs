098126802f06ae836a8aef358d8dd498596e49cd
iviraa <chetanchal.cs@gmail.com>
2024-12-11 19:44:33 -0600

    pwa configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
 /** @type {import('next').NextConfig} */
+
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA(nextConfig);
